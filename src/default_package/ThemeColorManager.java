18bf9db3f0f48b94779543acfa6b7bfbd672295a
Enes Kaya <72800099+eneskaya20@users.noreply.github.com>
2023-04-10 12:51:52 +0300

    Add files via upload

---
+++ src/default_package/ThemeColorManager.java
@@ -0,0 +1,17 @@
+package default_package;
+
+import javax.swing.JFrame;
+
+public class ThemeColorManager {
+    private IThemeColor iThemeColor;
+    public ThemeColorManager(IThemeColor iThemeColor){
+        this.iThemeColor = iThemeColor;
+    }
+    
+    public void setBackgroundColor(JFrame jFrame){
+        iThemeColor.setBackgroundColor(jFrame);
+    }
+    public void setFont(JFrame jFrame){
+        iThemeColor.setFont(jFrame);
+    }
+}
